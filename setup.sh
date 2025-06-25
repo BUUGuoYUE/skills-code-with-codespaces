2a8cf023f4b54c538fd5bc6bee0a770a00e52eee
BUUGuoYUE <jk138376@163.com>
2025-06-25 07:37:53 +0000

    Adding setup.sh from the codespace!

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get updat
+sudo apt-get install sl
+echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+git add setup.sh --chmod=+x
+git commit -m "Adding setup.sh from the codespace!"
+git push
+sl
+git add setup.sh --chmod=+x
